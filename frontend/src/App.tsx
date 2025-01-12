c0df2961a88213f7014d4e634919afb07c47391b
amirrajj-dev <amiramraja@gmail.com>
2025-01-12 18:29:29 +0330

    add some necessary images and svg and packages and react router dom routes for thi project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home/page'
+import SignUp from './pages/signup/page'
+import SignIn from './pages/signin/page'
+import Settings from './pages/settings/page'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/signup' element={<SignUp/>}></Route>
+        <Route path='/signin' element={<SignIn/>}></Route>
+        <Route path='/settings' element={<Settings/>}></Route>
+      </Routes>
+    </div>
   )
 }
 
